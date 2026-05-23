05df0014b4a306265c35a478255a62f4ea788823
Gaurav <gaurav@gmail.com>
2026-05-23 12:59:02 +0530

    clean project commit

--- Team_Task_Manager.py
+++ Team_Task_Manager.py
@@ -329,6 +329,12 @@ def dashboard():
         "completed_tasks": done,
         "pending_tasks": pending
     })
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
+
 # ==========================================
 # API TESTING EXAMPLES
 # ==========================================
